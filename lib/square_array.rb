f081e933e9d8c9af27b94b39e2061619b11f1bb6
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 01:21:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 new_array = []
 counter = 0
 def square_array(array)
-  array.length.times do |index|
+  while  array.length.times do |index|
    new_array.push(array[index]*array[index])
   end
 end
